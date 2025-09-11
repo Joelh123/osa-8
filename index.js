3c909142038c6678a99dd962fd7e4b284219025e
Joel H. <143587522+Joelh123@users.noreply.github.com>
2025-09-11 13:27:09 +0300

    8.6

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v1: uuid } = require("uuid");
 
 let authors = [
 	{
@@ -119,6 +120,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book!],
     allAuthors: [Author!]
   }
+
+  type Mutation {
+    addBook(
+      title: String!,
+      author: String!,
+      published: Int!,
+      genres: [String!]
+    ): Book!
+  }
 `;
 
 const resolvers = {
@@ -134,6 +144,14 @@ const resolvers = {
 				: newBooks;
 		},
 		allAuthors: () => authors,
+	},
+	Mutation: {
+		addBook: (root, args) => {
+			const book = { ...args, id: uuid() };
+			books = books.concat(book);
+			authors = authors.concat({ name: args.author, id: uuid() });
+			return book;
+		},
 	},
 	Author: {
 		bookCount: (root) =>
